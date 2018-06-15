5ad456acd06805799ded4623bec370be3d400e34
Prayer LBL <384850606@qq.com>
2018-06-15 18:15:49 +0800

    step x

---
+++ RazorPagesMoive/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMoive.Models;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMoive.Pages.Movies
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RazorPagesMoive.Models.RazorPagesMoiveContext _context;
+
+        public DetailsModel(RazorPagesMoive.Models.RazorPagesMoiveContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
--- RazorPagesMoive/Program.cs
+++ RazorPagesMoive/Program.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RazorPagesMovie.Models;
+using RazorPagesMoive.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace RazorPagesMoive
 {
@@ -14,9 +18,30 @@ namespace RazorPagesMoive
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<RazorPagesMoiveContext>();
+                    // requires using Microsoft.EntityFrameworkCore;
+                    context.Database.Migrate();
+                    // Requires using RazorPagesMovie.Models;
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
         }
-
+        public static IWebHost BuildWebHost(string[] args) =>
+WebHost.CreateDefaultBuilder(args)
+.UseStartup<Startup>()
+.Build();
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
